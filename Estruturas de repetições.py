e117be6d86ba186ecdb7ab5426b053411a80f290
LETICYA NUNES DE FRANCA <leticya.franca@aluno.senac.rio>
2025-09-01 21:12:08 -0300

    arquivos novos

---
+++ Estruturas de repetições.py
@@ -0,0 +1,78 @@
+'''contador = 1
+
+while contador <= 5:
+    print(f'Contador = {contador}')
+    contador += 1'''
+
+'''senha = ''
+
+while senha != '1234':
+    senha = input('Digite a senha: ').strip() #apagar espaços
+    if senha == '1234':
+        print('Acesso Liberado')
+    else:
+     print('Senha incorreta.Digite novamente')'''
+
+'''for i in range(0,31,10):
+    print(i)'''
+
+
+'''while True: 
+
+     numero = int(input('Digite um número maior que 10: '))
+       
+     if numero > 10:
+        print('Esse número é maior que 10')
+        break
+    
+     else:
+        print('Número inválido.Digite outro número')'''
+
+'''for i in range(1,11):
+    print(f'7 x {i} = {7*i}')'''
+
+
+'''contador = 0
+
+while contador <= 20:
+    print(contador)
+    contador +=2'''
+
+#cliente = ('joao', 22, 4800.50, 'Rua jota, N80') -- tupla
+#cliente = ['joao', 22, 4800.50, 'Rua jota, N80'] -- lista
+#print(cliente[2]) --- aparece o segundo indice (salario)
+    
+'''cliente = ['joao', 22, 4800.50, 'Rua jota, Nº80']
+
+telefone = 21985568641
+
+cliente.append(telefone)
+print(cliente)
+
+for i in cliente:
+    print(i)'''
+
+cliente = []
+
+while True:
+    try:
+        num = int(input('Digitr o Nº: '))
+        cliente.append(num)
+        sair = input('Deseja inserir mais um Nº?(S/N) ').upper().strip() #upper = tudo maiusculo/ strip = apagar espaços
+        if sair == 'N':
+            break
+    except:
+        print('Digite um Nº Válido')
+
+print('1\n2\n3\n4')
+
+'''while True:
+    try:
+        opcao = int(input('opção: '))
+        if opcao in [1,2,3,4]:
+            print('opção ok')
+            break
+        else: 
+            print('Opção inválida')
+    except:
+        print('Digite um número válido')'''
